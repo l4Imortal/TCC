8848d18b95935ce589abbd1a256e966ff7481b8c
s3idenada <162241906+s3idenada@users.noreply.github.com>
2025-05-16 19:10:15 -0300

    Add files via upload

---
+++ BD/estoque_saidas.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+--
+-- Host: localhost    Database: estoque
+-- ------------------------------------------------------
+-- Server version	8.0.42
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `saidas`
+--
+
+DROP TABLE IF EXISTS `saidas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `saidas` (
+  `ean` varchar(13) NOT NULL,
+  `produto` varchar(255) NOT NULL,
+  `quantidade` int NOT NULL,
+  `fornecedor` varchar(255) NOT NULL,
+  `nota_fiscal` varchar(50) NOT NULL,
+  `valor_unitario` decimal(10,2) NOT NULL,
+  `responsavel` varchar(255) NOT NULL,
+  `data_saida` date NOT NULL,
+  PRIMARY KEY (`ean`,`nota_fiscal`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `saidas`
+--
+
+LOCK TABLES `saidas` WRITE;
+/*!40000 ALTER TABLE `saidas` DISABLE KEYS */;
+INSERT INTO `saidas` VALUES ('1111111111111','Camisa',50,'Mercado Livre','111.111.111',150.00,'Enzo','2025-05-12'),('22222222222','Calça Jeans',60,'Mercado Livre','222.222.222',80.00,'Mateus','2025-05-11'),('33333333333','Blusa Moletom',40,'Mercado Livre','333.333.333',100.00,'Enzo','2025-05-13'),('4444444444444','Camisa',21,'Mercado Livre','432',12.00,'sim','2025-05-16');
+/*!40000 ALTER TABLE `saidas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-05-16 19:03:35
